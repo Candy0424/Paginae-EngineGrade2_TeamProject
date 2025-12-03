53b506f60d2bf65af3fa2978aa0fde83812800a5
PSB08 <Parkseongbin08@gmail.com>
2025-12-04 08:31:48 +0900

    feat : Plus1 settings

--- Assets/Work/PSB/Code/SaveSystem/StageProgressManager.cs
+++ Assets/Work/PSB/Code/SaveSystem/StageProgressManager.cs
@@ -104,7 +104,7 @@ namespace Work.PSB.Code.SaveSystem
 
         public int GetMaxStars()
         {
-            return 24;  //스테이지 수 * 3 방법이 생각안나서 그냥 넣음
+            return 27;  //스테이지 수 * 3 방법이 생각안나서 그냥 넣음
         }
         
         private void Update()
